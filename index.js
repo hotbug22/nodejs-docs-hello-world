13c03d65c4161915ace8213e26957cef5b848a57
Ubuntu <hotbug22@gmail.com>
2018-08-24 08:53:43 +0000

    indeks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-    response.end("something wronaaaaaaa");
+    response.end("myfirst app in azure");
 
 });
 
