0bcf4d7439645d0c5ead2f451166d78d210a6f44
Ubuntu <hotbug22@gmail.com>
2018-11-13 14:40:57 +0000

    function test

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-    response.end("you can make it!!!");
+    response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"})
+    response.end(sum(1,2,3));
+
 
 });
 
@@ -11,3 +12,11 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+
+function sum (param1, param2, param3){
+    return param1 + param2 + param3;
+}
+
+
+//
